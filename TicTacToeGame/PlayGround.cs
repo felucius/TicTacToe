11cd6179cebd7fa8cd67afe93ee73b368efb3da7
Maxime <maxime.de.lange@outlook.com>
2023-02-28 13:40:43 +0100

    Extended the playground windows forms class, so that it uses the game logic and less hardcoded values to determine the winner (logic wise)

--- TicTacToeGame/PlayGround.cs
+++ TicTacToeGame/PlayGround.cs
@@ -1,4 +1,3 @@
-using Microsoft.VisualBasic;
 using TicTacToe;
 
 namespace WinFormsApp1
@@ -12,16 +11,29 @@ namespace WinFormsApp1
         private Player humanPlayer;
         private Player computerPlayer;
 
+        private GameLogic gameLogic;
+        private readonly List<KeyValuePair<Button, int>> tiles;
+
         public PlayGround()
         {
             InitializeComponent();
+
+            // Initialize basic information
             humanPlayer = Player.X;
             computerPlayer = Player.O;
             randomSelection = new Random();
-            buttons = new List<Button> { btnTile1, btnTile2, btnTile3, btnTile6, btnTile5, btnTile4, btnTile9, btnTile8, btnTile7 };
+            buttons = new List<Button> { btnTile1, btnTile2, btnTile3, btnTile4, btnTile5, btnTile6, btnTile7, btnTile8, btnTile9 };
+            
+            // Create game logic
+            gameLogic = new GameLogic();
+            tiles = gameLogic.CreateBoard(buttons);
+
             RestartGame();
         }
 
+        /// <summary>
+        /// Clears the board and restarts the score
+        /// </summary>
         private void RestartGame()
         {
             playerScore = 0;
@@ -37,6 +49,9 @@ namespace WinFormsApp1
             }
         }
 
+        /// <summary>
+        /// Clears the board and start a new game
+        /// </summary>
         private void NextGame()
         {
             foreach (var button in buttons)
@@ -78,37 +93,27 @@ namespace WinFormsApp1
             }
         }
 
-        private void btnRestart_Click(object sender, EventArgs e)
-        {
-            RestartGame();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var action = (Button)sender;
-            // If the player has selected one tile, then the computer may randomly select a tile
-            PlayerClick(action);
-            StartComputer();
-        }
-
-        private void computerTimer_Tick(object sender, EventArgs e)
-        {
-            ComputerSelectionStupid();
-            StopComputer();
-            CheckWinner();
-        }
-
+        /// <summary>
+        /// Enables the CPU to make a move
+        /// </summary>
         private void StartComputer()
         {
             computerTimer.Enabled = true;
             computerTimer.Interval = 300;
         }
 
+        /// <summary>
+        /// Stops the CPU movement
+        /// </summary>
         private void StopComputer()
         {
             computerTimer.Enabled = false;
         }
 
+        /// <summary>
+        /// Adds the score to the designated player
+        /// </summary>
+        /// <param name="player">the player who has won</param>
         private void AddScore(Player player)
         {
             switch (player)
@@ -129,51 +134,84 @@ namespace WinFormsApp1
         /// </summary>
         private void CheckWinner()
         {
-            if (
-                // Horizontal checks
-                btnTile1.Text.Equals(StringConstants.PLAYER_ICON) && btnTile2.Text.Equals(StringConstants.PLAYER_ICON) && btnTile3.Text.Equals(StringConstants.PLAYER_ICON) ||
-                btnTile4.Text.Equals(StringConstants.PLAYER_ICON) && btnTile5.Text.Equals(StringConstants.PLAYER_ICON) && btnTile6.Text.Equals(StringConstants.PLAYER_ICON) ||
-                btnTile7.Text.Equals(StringConstants.PLAYER_ICON) && btnTile8.Text.Equals(StringConstants.PLAYER_ICON) && btnTile9.Text.Equals(StringConstants.PLAYER_ICON) ||
-                
-                // Vertical checks
-                btnTile1.Text.Equals(StringConstants.PLAYER_ICON) && btnTile4.Text.Equals(StringConstants.PLAYER_ICON) && btnTile7.Text.Equals(StringConstants.PLAYER_ICON) ||
-                btnTile2.Text.Equals(StringConstants.PLAYER_ICON) && btnTile5.Text.Equals(StringConstants.PLAYER_ICON) && btnTile8.Text.Equals(StringConstants.PLAYER_ICON) ||
-                btnTile3.Text.Equals(StringConstants.PLAYER_ICON) && btnTile6.Text.Equals(StringConstants.PLAYER_ICON) && btnTile9.Text.Equals(StringConstants.PLAYER_ICON) ||
+            var lines = gameLogic.CheckBoardLines();
+            var playerMoves = new List<int>();
+            var computerMoves = new List<int>();
+            var isMatchPlayer = false;
+            var isMatchCPU = false;
 
-                // Cross checks
-                btnTile1.Text.Equals(StringConstants.PLAYER_ICON) && btnTile5.Text.Equals(StringConstants.PLAYER_ICON) && btnTile9.Text.Equals(StringConstants.PLAYER_ICON) ||
-                btnTile3.Text.Equals(StringConstants.PLAYER_ICON) && btnTile5.Text.Equals(StringConstants.PLAYER_ICON) && btnTile7.Text.Equals(StringConstants.PLAYER_ICON))
+            foreach(var tile in tiles)
+            {
+                if(tile.Key.Text == StringConstants.PLAYER_ICON)
+                {
+                    playerMoves.Add(tile.Value);
+                }
+
+                if(tile.Key.Text == StringConstants.COMPUTER_ICON)
+                {
+                    computerMoves.Add(tile.Value);
+                }
+            }
+
+            foreach(var boardLine in lines)
+            {
+                foreach (var line in boardLine)
+                {
+                    // Comparing the moves against the board lines
+                    isMatchPlayer = line.All(x => playerMoves.Any(y => y == x));
+                    isMatchCPU = line.All(x => computerMoves.Any(y => y == x));
+
+                    if(isMatchPlayer || isMatchCPU)
+                    {
+                        break;
+                    }
+                }
+
+                if (isMatchPlayer || isMatchCPU)
+                {
+                    break;
+                }
+            }
+
+            if (isMatchPlayer)
             {
                 MessageBox.Show(StringConstants.PLAYER_WINS_MESSAGE);
                 AddScore(humanPlayer);
                 NextGame();
             }
-
-            if (
-                // Horizontal checks
-                btnTile1.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile2.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile3.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                btnTile4.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile5.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile6.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                btnTile7.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile8.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile9.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                
-                // Vertical checks
-                btnTile1.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile4.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile7.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                btnTile2.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile5.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile8.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                btnTile3.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile6.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile9.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                
-                // Cross checks
-                btnTile1.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile5.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile9.Text.Equals(StringConstants.COMPUTER_ICON) ||
-                btnTile3.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile5.Text.Equals(StringConstants.COMPUTER_ICON) && btnTile7.Text.Equals(StringConstants.COMPUTER_ICON))
+            else if (isMatchCPU)
             {
                 MessageBox.Show(StringConstants.COMPUTER_WINS_MESSAGE);
                 AddScore(computerPlayer);
                 NextGame();
             }
 
-            if(buttons.All(x => x.Text != String.Empty))
+            if (buttons.All(x => x.Text != string.Empty))
             {
                 MessageBox.Show(StringConstants.THE_MATCH_IS_A_TIE);
                 NextGame();
             }
+        }
+
+        // Events
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var action = (Button)sender;
+            // If the player has selected one tile, then the computer may randomly select a tile
+            PlayerClick(action);
+            StartComputer();
+        }
+
+        private void computerTimer_Tick(object sender, EventArgs e)
+        {
+            ComputerSelectionStupid();
+            StopComputer();
+            CheckWinner();
         }
     }
 }
